e5a13d793af0aacdd0d2544ae365175062e1009d
Muhammad Rabee <muhammad.rabee02@gmail.com>
2022-04-21 13:32:25 -0400

    changed config.js

--- config/config.js
+++ config/config.js
@@ -25,8 +25,8 @@ export const navigation = {
 	],
 }
 export const intro = {
-	title: "Hey, I'm Hashir",
-	description: "A Computer Engineer creating mobile apps and static websites.",
+	title: "Hey, I'm Muhammad",
+	description: "A Mathematics student studying data science and software engineering.",
 	image: profile.src,
 	buttons: [
 		{
